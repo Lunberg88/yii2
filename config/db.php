616999cfd60481f00e869eb657d9118913b9b2f6
lunberg88 <lunberg88@gmail.com>
2016-12-10 15:33:11 +0200

    dev project

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=newdb',
+    'dsn' => 'mysql:host=localhost;dbname=mydb',
     'username' => 'root',
     'password' => '280388',
     'charset' => 'utf8',
